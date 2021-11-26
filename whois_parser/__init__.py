36ddb6da3a858b090ebd504d3ada08ac72ee3132
Manabu Niseki <manabu.niseki@gmail.com>
2021-11-27 05:24:42 +0900

    fix: use importlib-metadata

--- whois_parser/__init__.py
+++ whois_parser/__init__.py
@@ -1,4 +1,8 @@
-import importlib.metadata as importlib_metadata
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
 
 from whois_parser.parser import WhoisParser  # noqa
 
